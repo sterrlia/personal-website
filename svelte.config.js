3b2bdab377ef063c17c5c17f3b1094435492d38a
sterrlia <dev@sterrlia.com>
2025-07-18 14:08:48 +0400

    deploy workflow

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
       fallback: "404.html",
     }),
     paths: {
-      base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
+      base: process.argv.includes("dev") ? "" : "/personal-website", //process.env.BASE_PATH,
     },
   },
 };
