4f71d1f7a6f735ab34c53c0eb3fdb869495f1f56
sterrlia <dev@sterrlia.com>
2025-07-18 18:26:00 +0400

    ats format

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -18,9 +18,9 @@
 {:else}
     <h2>About me</h2>
     <p>
-        I&nbsp;am 23&nbsp;y&rsquo;o backend developer with 5&nbsp;years
-        experience of&nbsp;development in PHP. I&nbsp;graduated in&nbsp;2020 with a&nbsp;degree
-        in&nbsp;programming and have been building web applications and backend
+        I am 23 years old backend developer with 5 years
+        experience of development in PHP. I graduated in 2020 with a degree
+        in programming and have been building web applications and backend
         systems ever since.
     </p>
 {/if}
